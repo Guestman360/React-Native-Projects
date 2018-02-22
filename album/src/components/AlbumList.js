565770b17baef8a63645a1dc16fc3e9323888008
Matt <mguest55@yahoo.com>
2018-02-21 23:23:59 -0800

    Imported axios for networking

--- album/src/components/AlbumList.js
+++ album/src/components/AlbumList.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
-    render() {
+    componentWillMount() { // Lifecycle method, this is imilar to viewDidAppear
+        console.log('Fuck bitches, get money')
+    }
+// https://rallycoding.herokuapp.com/api/music_albums
+    render() { // All class based components must have render method to return some JSX
         return (
             <View>
                 <Text>Album List !!!</Text>
             </View>
         );
-    };
-};
+    }
+}
 
 export default AlbumList;
