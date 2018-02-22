4272f09e9989bfa391880f8dde8078be0c5dbd87
Matt <mguest55@yahoo.com>
2018-02-21 23:56:19 -0800

    Added AlbumDetail file.

--- album/src/components/AlbumList.js
+++ album/src/components/AlbumList.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import AlbumDetail from './src/components/AlbumDetail';
+import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] };
+
     componentWillMount() { // Lifecycle method, this is imilar to viewDidAppear
-        console.log('Fuck bitches, get money')
+        // Requst is asyncronous so it is a promise, fulfill by calling .then statement 
+        // then is called when request is complete
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
     }
-// https://rallycoding.herokuapp.com/api/music_albums
+
+    renderAlbums() {
+        // Reference javascript variable requires { }
+        return this.state.albums.map(album => 
+            <Text key={album.title}>{album.title}</Text>
+        );
+    }
+
     render() { // All class based components must have render method to return some JSX
         return (
             <View>
-                <Text>Album List !!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
