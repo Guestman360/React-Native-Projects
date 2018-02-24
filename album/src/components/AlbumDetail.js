8721c9a775d378179bac5c2f533efa0bce373dcc
Matt <mguest55@yahoo.com>
2018-02-23 22:56:26 -0800

    Added CardSection component.

--- album/src/components/AlbumDetail.js
+++ album/src/components/AlbumDetail.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
+// Whatever is passed here is props.children for CardSection tags (Referring to the Text tag with props.album.title)
 const AlbumDetail = (props) => {
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>
+                <Text>{props.album.title}</Text> 
+            </CardSection>
         </Card>
     );
 };
---
+++ album/src/components/CardSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// https://codeburst.io/a-quick-intro-to-reacts-props-children-cb3d2fce4891
+const CardSection = (props) => { // Use props.children when 'compnenets don’t know their children ahead of time'
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+};
+
+const styles = {
+    containerStyle: {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative'
+    }
+};
+
+export default CardSection;
