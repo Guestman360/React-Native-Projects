d0b410846bc6fa444116ea950668f7c298d7d53c
Matt <mguest55@yahoo.com>
2018-02-23 23:32:29 -0800

    The cell is coming together and looking nicer.

--- album/src/components/AlbumDetail.js
+++ album/src/components/AlbumDetail.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 // Whatever is passed here is props.children for CardSection tags (Referring to the Text tag with props.album.title)
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => { 
+    // Destructuring looks like this
+    const { title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headercontentStyle } = styles;
+
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text> 
+                <View>
+                    <Image 
+                        source={{ uri: thumbnail_image }} 
+                        style={thumbnailStyle}
+                    />
+                </View>
+                <View style={headercontentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View> 
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headercontentStyle: {
+        flexDirection:'column',
+        justifyContent:'space-around'
+    },
+    thumbnailStyle: { // Images must have size in order to show on screen
+        height: 50,
+        width: 50
+    }
 };
 
 export default AlbumDetail;
--- album/src/components/AlbumList.js
+++ album/src/components/AlbumList.js
@@ -10,9 +10,9 @@ class AlbumList extends Component {
         // Requst is asyncronous so it is a promise, fulfill by calling .then statement 
         // then is called when request is complete
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-        .then(response => this.setState({ albums: response.data }));
+        .then(response => this.setState({ albums: response.data })); // passes the response data to albums variable
     }
-
+    // The property names are dictated by the json keys, know the json in order to know the proper keys to use
     renderAlbums() {
         // Reference javascript variable requires { }
         return this.state.albums.map(album => 
