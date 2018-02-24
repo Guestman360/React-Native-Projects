ac97c3ad00eadbbfce4470a81ef1fa341c7ab76b
Matt <mguest55@yahoo.com>
2018-02-23 23:46:52 -0800

    Added image to cell

--- album/src/components/AlbumDetail.js
+++ album/src/components/AlbumDetail.js
@@ -6,22 +6,34 @@ import CardSection from './CardSection';
 // Whatever is passed here is props.children for CardSection tags (Referring to the Text tag with props.album.title)
 const AlbumDetail = ({ album }) => { 
     // Destructuring looks like this
-    const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headercontentStyle } = styles;
+    const { title, artist, thumbnail_image, image } = album;
+    const { 
+        thumbnailStyle, 
+        headercontentStyle,
+        headerTextStyle,
+        thumbnailContainerStyle,
+        imageStyle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         source={{ uri: thumbnail_image }} 
                         style={thumbnailStyle}
                     />
                 </View>
                 <View style={headercontentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View> 
+            </CardSection>
+            <CardSection>
+                <Image 
+                    source={{ uri: image }} 
+                    style={imageStyle}
+                />
             </CardSection>
         </Card>
     );
@@ -32,9 +44,23 @@ const styles = {
         flexDirection:'column',
         justifyContent:'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: { // Images must have size in order to show on screen
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: { 
+        justifyContent:'center',
+        alignItems:'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
