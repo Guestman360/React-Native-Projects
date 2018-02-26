e398c6c1324b38c1401f7a228a558d78db234889
Matt <mguest55@yahoo.com>
2018-02-26 01:28:22 -0800

    Moved App.js to src folder

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
+
+// index.js is mandatory but you can change App.js, App.js is only a defualt root view
 
 AppRegistry.registerComponent('auth', () => App);
