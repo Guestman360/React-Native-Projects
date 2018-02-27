c6d9fe81819d0e850bd4117f8b9804068fc32456
Matt <mguest55@yahoo.com>
2018-02-26 23:35:43 -0800

    Added a couple new files.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentDidMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBc8RgRvOda0tIZU-xpkeJ5iJm_xsKPFK0",
+            authDomain: "auth-11576.firebaseapp.com",
+            databaseURL: "https://auth-11576.firebaseio.com",
+            projectId: "auth-11576",
+            storageBucket: "auth-11576.appspot.com",
+            messagingSenderId: "964484583691"
+        });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>Hey there!</Text>
+                <LoginForm>
+
+                </LoginForm>
             </View>
         );
     }
---
+++ auth/src/components/common/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const Input = ({ label }) => {
+    render {
+        return (
+            <View>
+                <Text></Text>
+            </View>
+        );
+    };
+};
+
+export { Input };
