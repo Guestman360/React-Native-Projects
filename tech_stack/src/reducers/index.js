9fc8b94c68e25a22b282ea015ef19bd74c3779ff
Matt <mguest55@yahoo.com>
2018-03-01 23:39:13 -0800

    Setup reducer and added dummy json file.

--- tech_stack/src/reducers/index.js
+++ tech_stack/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 // libraries is a reducer that always returns an array
 // Remeber! Reducers must always return something
 export default combineReducers({
-    libraries: () => []
+    libraries: LibraryReducer
 });
