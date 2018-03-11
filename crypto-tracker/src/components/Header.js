4bc9d0c4b6784c7fdb367a8a9f9fc8a5f28fbf89
Matt <mguest55@yahoo.com>
2018-03-10 23:43:06 -0800

    Added a few libraries for redux and also added header to app

---
+++ crypto-tracker/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// This is a stateless componenet because it is just used to style, in this case the app's header
+const Header = () => {
+    return (
+        <View style={headerContainer}>
+            <Text style={header}>
+                Cryptocurrency app
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        display: "flex",
+        marginTop: 55,
+        alignItems: "center"
+    },
+    header: {
+        fontWeight: "bold",
+        fontSize: 20
+    }
+})
+
+const { headerContainer, header } = styles;
+
+// DONT FORGET TO EXPORT!!!
+export default Header;
