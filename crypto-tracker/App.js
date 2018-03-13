a9b84c40f82a48ebd34cbfaa2060e43055dfdd55
Matt <mguest55@yahoo.com>
2018-03-12 23:43:40 -0700

    exported necessary files to have access in App.js file

--- crypto-tracker/App.js
+++ crypto-tracker/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { Header } from './src/components';
+
+import { Header, CryptoContainer } from './src/components';
 import Store from './src/Store'
 
 export default class App extends React.Component {
@@ -10,17 +11,9 @@ export default class App extends React.Component {
       <Provider store={Store}>
         <View >
           <Header />
+          <CryptoContainer />
         </View>
       </Provider>
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
